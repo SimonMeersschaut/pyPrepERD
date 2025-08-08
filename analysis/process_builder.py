5b9abcc01bb4e4b82cd0f8f09c7885d7cf43925d
simon meersschaut <simon.meersschaut@gmail.com>
2025-08-08 14:14:54 +0200

    Update LTOA files

---
+++ analysis/process_builder.py
@@ -0,0 +1,9 @@
+from analysis.processes.process import Process
+
+
+class ProcessBuilder(Process):
+    def build(self):
+        ...
+    
+    def run(self):
+        ...
---
+++ tests/analysis/processes/ltoa/test_LtoaParser.py
@@ -0,0 +1,23 @@
+import unittest
+from analysis.processes.ltoa import LtoaParser
+
+MAX_NADC = 16
+ADC_SIZE = 2  # bytes per ADC value
+NFBYTES = 1048576  # 1 MB
+TXTFILE = "output.txt"
+MAXEVENT = 1000000
+
+class TestPreprocessing(unittest.TestCase):
+    def test_preprocess_data(self):
+        # read expected result
+        with open("tests/analysis/processes/ltoa/expected.txt", 'r') as f:
+            flt_expected_lines = f.read().split(' \n')[:-1]
+
+        # Generate cumputed result
+        df = LtoaParser("tests/analysis/processes/ltoa/ERD16_075_01A.lst")
+        flt_computed_lines = df.parse() #.split('\n')
+
+        self.assertEqual(len(flt_expected_lines), len(flt_computed_lines), "Nunber of lines did not match expected value.")
+
+        for i in range(len(flt_expected_lines)):
+            self.assertEqual(flt_expected_lines[i], flt_computed_lines[i], f"Line {i} did not match the expected output.")
